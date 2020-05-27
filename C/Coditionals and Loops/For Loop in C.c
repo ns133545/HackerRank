c2dea1eb15ed8e1a985a20672060b772fc5e70ce
Nitin Sharma <56151582+ns133545@users.noreply.github.com>
2020-05-27 18:07:07 +0530

    Create For Loop in C.c

---
+++ C/Coditionals and Loops/For Loop in C.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+
+int main() 
+{
+    int a, b,i;
+    scanf("%d\n%d", &a, &b);
+  	// Complete the code.
+ 
+
+      for(i=a;i<=b;i++)
+      {
+               if(i<10)
+{
+    switch(i)
+              {
+                  case 1: printf("one\n");
+                  
+                  case 2: printf("two\n");
+                  if(b==2)
+                  {break;}
+                  
+                  case 3: printf("three\n");
+                  
+                  case 4: printf("four\n");
+                  
+                  case 5: printf("five\n");
+                  
+                  case 6: printf("six\n");
+                  
+                  case 7: printf("seven\n");
+                  
+                  case 8: printf("eight\n");
+    
+                  case 9: printf("nine\n");
+                  break;
+
+              }break;
+}
+      }
+          
+for(i=10;i<=b;i++)
+{
+      if(i%2==0)
+          { printf("even\n");
+
+          }
+          else
+          {printf("odd\n");
+
+          }      
+          
+}
+
+    return 0;
+}
+
