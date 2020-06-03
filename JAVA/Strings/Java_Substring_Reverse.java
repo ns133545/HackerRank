3e48bf4d4ec1654d20ece761a8b4de17822db552
Nitin Sharma <56151582+ns133545@users.noreply.github.com>
2020-06-03 14:54:33 +0530

    Create Java_Substring_Reverse.java

---
+++ JAVA/Strings/Java_Substring_Reverse.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.nextLine();
+        /* Enter your code here. Print output to STDOUT. */
+        String reversed = new StringBuilder(A).reverse().toString();
+        if(A.equals(reversed))
+        {
+            System.out.println("Yes");
+
+        }
+        else
+        {
+            System.out.println("No");
+        }
+        
+    }
+}
