f0d473a4ef18f96ba1725f385f46759b6685f0e5
Nitin Sharma <56151582+ns133545@users.noreply.github.com>
2020-06-03 14:53:18 +0530

    Create Java_Substring_Comparison.java

---
+++ JAVA/Strings/Java_Substring_Comparison.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Solution {
+ 
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = s.substring(0, k);
+        String largest  = s.substring(0, k);
+
+        for (int i = 0; i <= s.length() - k; i++) {
+            String curr = s.substring(i, i + k);
+            if (smallest.compareTo(curr) > 0){
+                smallest = curr;
+            }
+            if (largest.compareTo(curr) < 0) {
+                largest = curr;
+            }
+        }
+
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        
+        return smallest + "\n" + largest;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
